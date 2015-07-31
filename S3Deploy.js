3f54a5fbf849fa491c119af04a42dacabf366eee
Daniel Ortega <daniel@gatero.mx>
2015-07-31 14:11:47 -0500

    method: Create a main method for upload all file types

--- S3Deploy.js
+++ S3Deploy.js
@@ -25,18 +25,40 @@ function noParamsGiven() {
   process.exit(-1);
 }
 
+function getFileList(path) {
+  var i, fileInfo, filesFound;
+  var fileList = [];
+
+  filesFound = fs.readdirSync(path);
+  for (i = 0; i < filesFound.length; i++) {
+    fileInfo = fs.lstatSync(path + filesFound[i]);
+    if (fileInfo.isFile()) fileList.push(filesFound[i]);
+  }
+  return fileList;
+}
+
+function getContentTypeByFile(fileName) {
+  var rc = 'application/octet-stream';
+  var fileNameLowerCase = fileName.toLowerCase();
+
+  if (fileNameLowerCase.indexOf('.html') >= 0) rc = 'text/html';
+  else if (fileNameLowerCase.indexOf('.css') >= 0) rc = 'text/css';
+  else if (fileNameLowerCase.indexOf('.json') >= 0) rc = 'application/json';
+  else if (fileNameLowerCase.indexOf('.js') >= 0) rc = 'application/x-javascript';
+  else if (fileNameLowerCase.indexOf('.png') >= 0) rc = 'image/png';
+  else if (fileNameLowerCase.indexOf('.jpg') >= 0) rc = 'image/jpg';
+
+  return rc;
+}
+
 function showUsage() {
   console.log('Use choosing one of these command line parameters:');
-  console.log('  audio folderName');
-  console.log('  code');
-  console.log('  createBucket');
-  console.log('  css');
-  console.log('  index');
-  console.log('  images');
-  console.log('  list');
-  util.log(typeof(config));
-  util.log(util.isArray(config));
-  util.log(config.BUCKET_NAME);
+  console.log('create_bucket');
+  console.log('all');
+  console.log('styles');
+  console.log('scripts');
+  console.log('images');
+  console.log('bower');
 }
 // end no params case
 
@@ -54,106 +76,28 @@ function uploadFile(remoteFilename, fileName) {
     Body: fileBuffer,
     ContentType: metaData
   }, function(error, response) {
-    console.log('uploaded file[' + fileName + '] to [' + remoteFilename + '] as [' + metaData + ']');
-    console.log(arguments);
+    util.log('uploaded file[' + fileName + '] to [' + remoteFilename + '] as [' + metaData + ']');
   });
 }
 
 function runWithParams() {
   console.log('S3 Deployer ... running option is [' + process.argv[2] + ']');
   switch(process.argv[2]) {
-    case 'index': uploadIndex(); break;
-    case 'list': uploadList(); break;
-    case 'code': uploadCode(); break;
-    case 'css': uploadCss(); break;
-    case 'images': uploadImages(); break;
-    case 'audio': uploadAudio(process.argv[3]); break;
-    case 'createBucket': createBucket(BUCKET_NAME); break;
+    case 'create_bucket': createBucket(BUCKET_NAME); break;
+    case 'styles': uploadSource('styles'); break;
+    case 'scripts': uploadSource('scripts'); break;
+    case 'images': uploadSource('images'); break;
+    case 'bower': uploadSource('bower'); break;
+    case 'upload_all':
+      uploadSource('styles');
+      uploadSource('scripts');
+      uploadSource('images');
+      uploadSource('bower');
+    break;
+
     default: console.log('...that option isn\'t recognized');
   }
 }
-
-
-function uploadAudio(folderName) {
-  var CODE_PATH = 'resources/audio/';
-  var fileList = getFileList('./' + CODE_PATH + folderName + '/');
-
-  fileList.forEach(function(entry) {
-    console.log(CODE_PATH + folderName + '/' + entry + ' >>> ' + './' + CODE_PATH + folderName + '/' + entry);
-    uploadFile(CODE_PATH + folderName + '/' + entry, './' + CODE_PATH + folderName + '/' + entry);
-  });
-}
-
-
-function uploadIndex() {
-  uploadFile('index.html', 'index.html');
-}
-
-
-function uploadList() {
-  uploadFile('resources/lists/options.json', './resources/lists/options.json');
-}
-
-
-function uploadCode() {
-  var CODE_PATH = 'resources/code/';
-  var fileList = getFileList('./' + CODE_PATH);
-
-  fileList.forEach(function(entry) {
-    uploadFile(CODE_PATH + entry, './' + CODE_PATH + entry);
-  });
-}
-
-
-function uploadCss() {
-  var CODE_PATH = 'resources/css/';
-  var fileList = getFileList('./' + CODE_PATH);
-
-  fileList.forEach(function(entry) {
-    uploadFile(CODE_PATH + entry, './' + CODE_PATH + entry);
-  });
-}
-
-
-function uploadImages() {
-  var CODE_PATH = 'resources/images/';
-  var fileList = getFileList('./' + CODE_PATH);
-
-  fileList.forEach(function(entry) {
-    uploadFile(CODE_PATH + entry, './' + CODE_PATH + entry);
-  });
-}
-
-
-function getFileList(path) {
-  var i, fileInfo, filesFound;
-  var fileList = [];
-
-  filesFound = fs.readdirSync(path);
-  for (i = 0; i < filesFound.length; i++) {
-    fileInfo = fs.lstatSync(path + filesFound[i]);
-    if (fileInfo.isFile()) fileList.push(filesFound[i]);
-  }
-
-  return fileList;
-}
-
-
-
-function getContentTypeByFile(fileName) {
-  var rc = 'application/octet-stream';
-  var fileNameLowerCase = fileName.toLowerCase();
-
-  if (fileNameLowerCase.indexOf('.html') >= 0) rc = 'text/html';
-  else if (fileNameLowerCase.indexOf('.css') >= 0) rc = 'text/css';
-  else if (fileNameLowerCase.indexOf('.json') >= 0) rc = 'application/json';
-  else if (fileNameLowerCase.indexOf('.js') >= 0) rc = 'application/x-javascript';
-  else if (fileNameLowerCase.indexOf('.png') >= 0) rc = 'image/png';
-  else if (fileNameLowerCase.indexOf('.jpg') >= 0) rc = 'image/jpg';
-
-  return rc;
-}
-
 
 function createBucket(bucketName) {
   s3.createBucket({Bucket: bucketName}, function() {
@@ -161,5 +105,13 @@ function createBucket(bucketName) {
     console.log(arguments);
   });
 }
+
+function uploadSource(type) {
+  var file_list = getFileList(config[type].local);
+
+  file_list.forEach(function(file){
+    uploadFile(config[type].remote + file, config[type].local + file);
+  });
+};
 
 
